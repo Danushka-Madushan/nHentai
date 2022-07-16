bf2c0c1a9ebad6faf650234e1def9c77c0cae62a
Danushka-Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2022-07-16 16:15:37 +0530

    fake site replaced .to with .net
    all regex updated
    new search methods were used
    artist name and Manga Misc is provided to the user

--- Main.py
+++ Main.py
@@ -1,41 +1,46 @@
 import requests
-import msvcrt
-import sys
 import re
+import json
+from datetime import datetime
+from pycookie import chromecookie
 
 try:
     ID = input("\n Enter [ Number in URL ] ID : ")
 
     if ID:
-        url = (f"https://nhentai.to/g/{ID}")
-        webdata = requests.get(url).content.decode("utf-8")
-        if re.search(r'<div class="code">\D+404[ ]*</div>', webdata):
+        url = (f"https://nhentai.net/g/{ID}/")
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+        }
+        webdata = requests.get(url, cookies=chromecookie('nhentai.net').cookies(), headers=headers).text
+        if '404 - Not Found' in webdata:
             print("\n Seems to be an INVALID ID Number Check Again!")
-            msvcrt.getch()
-            sys.exit()
         else:
-
-            title = re.search(r'<div id="info">\D+<h1>(.+?)</h1>', webdata).group(1)
-            copyright = re.search(r'<div class="tag-container field-name ">\D+Parodies\D+<span class="tags">\D+class="tag tag-[0-9]* ">(.+?)</a>', webdata).group(1)
-            up_time = re.search(r'<div>Uploaded <time datetime="([0-9-]*)', webdata).group(1)
-            pages = re.search(r'<div>([0-9]*) pages</div>', webdata).group(1)
-            language = re.search(r'<div class="tag-container field-name ">\D+Languages\D+<span class="tags">\D+class="tag tag-[0-9]* ">(.+?)</a>', webdata).group(1)
-            raw_strings = re.findall(r'data-src="(https?://[A-z.]*/galleries/[0-9]*/[0-9]*)t(.[a-z]*)"', webdata)
+            raws = re.search(r'JSON.parse.{2}({.+})', webdata).group(1)
+            JsonData = json.loads(raws.encode('utf-8').decode('unicode-escape'))
+            with open('%s.json' % ID, 'w') as file:file.write(json.dumps(JsonData, indent=4))
+            title = re.search(r'class="before">(?P<artist>.*)</span>[^=]+="pretty">(?P<title>.+)</span><span class="after">(?P<misc>.*)</span></h1>', webdata)
+            language = re.findall(r'<a href="/language/([^/]+)', webdata)
+            raw_strings = re.findall(r'data-src="(https?)://[A-z]+([0-9]+).{25,35}/(?:[0-9]+[A-z]+).([^"]+)', webdata)
             print("\n -------------------------------------")
-            print(f" : Manga Name : {title}")
+            print(f" : Manga Name : {title.group('title')}")
             print(" -------------------------------------")
-            print(f" : Language   : {language.upper()}")
+            print(f" : Artist     : {title.group('artist')}")
             print(" -------------------------------------")
-            print(f" : Parodies   : {copyright.upper()}")
+            print(f" : Manga Info :{title.group('misc')}")
             print(" -------------------------------------")
-            print(f" : Uploaded   : {up_time}")
+            print(f" : Language   : {[x.upper() for x in language]}")
             print(" -------------------------------------")
-            print(f" : Pages      : {pages}")
+            print(f" : Uploaded   : {datetime.fromtimestamp(JsonData['upload_date'])}")
+            print(" -------------------------------------")
+            print(f" : Pages      : {JsonData['num_pages']}")
             print(" -------------------------------------")
 
-            links = []
+            links, star = [], 1
             for x in raw_strings:
-                links.append(x[0]+x[1])
+                link = "%s://i%s.nhentai.net/galleries/%s/%s.%s" % (x[0], x[1], JsonData['media_id'], star, x[2])
+                links.append(link)
+                star+=1
             print(f" : Images     : {len(links)}")
             print(" -------------------------------------")
             m = open(f"Links [{ID}].txt", "w")
@@ -44,14 +49,9 @@ try:
             m.close()
             print(" : All Links to Images Saved Locally!")
             print(" -------------------------------------")
-            msvcrt.getch()
-            sys.exit()
 
     else:
-        print("\n Enter Following [ https://nhentai.to/g/{ID}! ] :)")
-        msvcrt.getch()
-        sys.exit()
+        print("\n Enter Following [ https://nhentai.net/g/{ID}! ] :)")
 
-except Exception as error:
+except NameError as error:
     print(f"\n Oops! This Happened : {error}")
-    msvcrt.getch()
